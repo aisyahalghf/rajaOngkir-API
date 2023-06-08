ee63ca3886b95263a6b93ef72ed02873622e70d1
aisyahalghf <alghifariaisyahputri@gmail.com>
2023-06-08 22:07:17 +0700

    vercel

--- routers/index.js
+++ routers/index.js
@@ -1,9 +1,5 @@
 const rajaOngkirRouter = require("./rajaOngkirRouter");
-// const busSeatRouter = require('./busSeatRouter');
-// const formationsRouter = require("./formationsRouter");
 
 module.exports = {
   rajaOngkirRouter,
-  // busSeatRouter,
-  //   formationsRouter,
 };
